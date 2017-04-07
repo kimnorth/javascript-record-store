f35149461fc1477e386da675d6a1934ea21409e1
kimnorth <benignhumour@gmail.com>
2017-04-07 19:31:18 +0100

    Customer can add a record to their collection.

--- customer.js
+++ customer.js
@@ -1,6 +1,15 @@
 var Customer = function(name, balance){
   this.name = name;
   this.balance = balance;
+  this.collection = [];
+}
+
+Customer.prototype = {
+
+  buyRecord: function(record){
+    this.collection.push(record);
+  }
+
 }
 
 module.exports = Customer;
--- specs/customer.specs.js
+++ specs/customer.specs.js
@@ -1,16 +1,24 @@
 var assert = require('assert');
 var Customer = require('../customer.js');
+var Record = require('../record.js');
 
 describe('Customer tests', function(){
 
   var customer;
+  var record;
 
   beforeEach(function(){
     customer = new Customer("Bob", 1000);
+    record = new Record("Iggy Pop", "Iggy Pop", "Rock", 1000);
   })
 
   it('Can have a balance', function(){
     assert.strictEqual(1000, customer.balance);
+  })
+
+  it('Can buy records', function(){
+    customer.buyRecord(record);
+    assert.strictEqual(1, customer.collection.length);
   })
 
 })
